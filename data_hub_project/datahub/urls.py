e085fe4a38b1c2b8dd49cdb7783473ece9233b29
Simar710 <simardeepsingh710@gmail.com>
2025-03-01 20:15:46 -0500

    Pushed some docker improvements and sentry error logins

--- data_hub_project/datahub/urls.py
+++ data_hub_project/datahub/urls.py
@@ -1,10 +1,11 @@
 # datahub/urls.py
 
 from django.urls import path
-from .views import DatasetListView, DatasetCreateView, DatasetListAPI
+from .views import DatasetListView, DatasetCreateView, DatasetListAPI, trigger_error
 
 urlpatterns = [
     path('', DatasetListView.as_view(), name='dataset-list'),
     path('add/', DatasetCreateView.as_view(), name='dataset-add'),
     path('api/datasets/', DatasetListAPI.as_view(), name='dataset-list-api'),
+    path("sentry-test/", trigger_error, name="sentry-test"),
 ]
--- data_hub_project/datahub/views.py
+++ data_hub_project/datahub/views.py
@@ -5,6 +5,8 @@ from django.views.generic import ListView, CreateView
 from .models import Dataset
 from rest_framework import generics
 from .serializers import DatasetSerializer
+from django.http import HttpResponse
+import sentry_sdk
 
 class DatasetListView(ListView):
     model = Dataset
@@ -20,3 +22,7 @@ class DatasetCreateView(CreateView):
 class DatasetListAPI(generics.ListCreateAPIView):
     queryset = Dataset.objects.all()
     serializer_class = DatasetSerializer
+
+def trigger_error(request):
+    sentry_sdk.capture_message("This is a test error sent to Sentry!", level="error")
+    return HttpResponse("Test error sent to Sentry!")
